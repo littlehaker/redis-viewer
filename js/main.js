c187a91891b716b2fd5c84df22c6cd5713617e0d
Young <young@E1.(none)>
2013-06-05 23:02:54 +0800

    实现了一个cli

---
+++ js/main.js
@@ -0,0 +1,43 @@
+var Redis = require('redis');
+var client = Redis.createClient();
+
+client.on('error', alert);
+
+// client.send_command('keys', ['*'], function(err, reply){
+//   console.log(err, reply);
+// });
+
+
+$(function(){
+  function send_command(){
+    var cmd = $('input').val();
+    var $pre = $('pre');
+    if(cmd !== ''){
+      var args = cmd.split(' ');
+      console.log(args);
+      var command = args[0];
+      args.splice(0, 1);
+      console.log(command, args);
+      client.send_command(command, args, function(err, reply){
+	$('input').focus().val('');
+	console.log(err, reply);
+	// $('pre').html(reply);
+	$pre.html('Command: ' + cmd + '</br></br>');
+	if(typeof(reply) === 'string'){
+	  $pre.append(reply + '</br>');
+	}
+	else{
+	  for(var i in reply){
+	    $pre.append(i + ':  ' + reply[i] + '</br>');
+	  }
+	}
+      });
+    }
+  }
+  $('button').click(send_command);
+  $('input').keydown(function(e){
+    if(e.keyCode === 13){
+      send_command();
+    }
+  }).focus();
+});
